dd9b46a0e6b5309298022cf096e591c676bbe7eb
Ruvaim <ruvaim51@gmail.com>
2023-01-04 17:08:37 +0530

    Added link in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-    return(
-        <div style={{textAlign:'center' , marginBottom: 10}}>
-            Made with ❤ by Ruvaim Kash
-        </div>
-    )
-}
+  return (
+    <div style={{ textAlign: 'center', marginBottom: 10 }}>
+      Made with ❤ by{' '}
+      <a href="https://ruvaim.me/" target="_blank">
+        Ruvaim Kash
+      </a>
+    </div>
+  );
+};
 
 export default Footer;
